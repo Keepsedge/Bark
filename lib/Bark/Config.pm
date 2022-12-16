c9e663068688b5e17754f0cfb8c3eb7aed1c5f6e
J.Sullivan <energy.keeper@gmail.com>
2022-12-16 02:09:39 -0500

    Update Config.pm
    Added methods to store and write config files.

--- lib/Bark/Config.pm
+++ lib/Bark/Config.pm
@@ -2,7 +2,6 @@ package Bark::Config;
 use strict;
 use warnings;
 use Carp qw[carp];
-use JSON::XS;
 
 # Generic Config Handler for Bark Plugins to use.
 
@@ -24,4 +23,41 @@ sub setConfigFile
     $self->{_configfile} = $filename;
 }
 
+sub writeToFile
+{
+    my $self = shift;
+    open(FH, ">+", $self->{_configfile}) or carp "Can not open config file.";
+    $self->_sort();
+    foreach my $key (keys(%{$self->{_config}}))
+    {  
+        print FH ($key."=".$self->getValue($key)."\n");
+    }
+    close FH;
+}
 
+sub _sort
+{
+    my $self = shift;
+    my @keys = sort(keys($self->{_config}));
+    my %config = ();
+    foreach my $key (@keys) {
+        $config->{$key} = $self->{_config}->{$key};
+    }
+    $self->{_config} = $config;
+}
+
+sub setValue
+{
+    my $self = shift;
+    my ($key, $value) = @_;
+
+    $self->{_config}->{$key} = $value;
+    $self->_sort();
+}
+
+sub getValue
+{
+    my $self = shift;
+    my $key = shift;
+    return $self->{_config}->{$key};
+}
